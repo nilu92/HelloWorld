2a9fd8a91b9be6403f20d1881dc3c0dbece2e4e1
nilu92 <57625806+nilu92@users.noreply.github.com>
2020-05-11 15:04:04 +0200

    Add files via upload

---
+++ HelloWorld.js
@@ -0,0 +1,13 @@
+
+window.onload= function (){
+const button = document.getElementById('bt1');
+var req = new XMLHttpRequest();
+var URLhost = 'https://swapi.co/api/planets/'
+
+
+///alert('Hello world');
+button.addEventListener('click',function()
+{
+  alert('Hello World')
+})
+    }
